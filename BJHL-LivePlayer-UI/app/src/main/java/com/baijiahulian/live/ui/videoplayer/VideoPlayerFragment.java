477d1659f174f1474a0e9006c97e62b4fdc81bff
boomheap <wangkangfei@yeah.net>
2017-05-04 15:10:55 +0800

    下行视频name

--- BJHL-LivePlayer-UI/app/src/main/java/com/baijiahulian/live/ui/videoplayer/VideoPlayerFragment.java
+++ BJHL-LivePlayer-UI/app/src/main/java/com/baijiahulian/live/ui/videoplayer/VideoPlayerFragment.java
@@ -2,11 +2,16 @@ package com.baijiahulian.live.ui.videoplayer;
 
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.baijiahulian.avsdk.liveplayer.GLTextureView;
 import com.baijiahulian.avsdk.liveplayer.ViERenderer;
+import com.baijiahulian.live.ui.R;
 import com.baijiahulian.live.ui.base.BaseFragment;
 
 /**
@@ -15,8 +20,10 @@ import com.baijiahulian.live.ui.base.BaseFragment;
 
 public class VideoPlayerFragment extends BaseFragment implements VideoPlayerContract.View {
 
-    VideoPlayerContract.Presenter presenter;
+    private VideoPlayerContract.Presenter presenter;
     private GestureDetector gestureDetector;
+    private FrameLayout flContainer;
+    private TextView tvName;
 
     @Override
     public void setPresenter(VideoPlayerContract.Presenter presenter) {
@@ -31,16 +38,31 @@ public class VideoPlayerFragment extends BaseFragment implements VideoPlayerCont
 
     @Override
     protected View getContentView() {
-        if (view == null) {
-            view = ViERenderer.CreateRenderer(getActivity(), true);
-        }
-        return view;
+        flContainer = new FrameLayout(getActivity());
+        ViewGroup.LayoutParams flLp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        flContainer.setLayoutParams(flLp);
+        //视频
+        View textureView = ViERenderer.CreateRenderer(getActivity(), true);
+        flContainer.addView(textureView);
+        //名字
+        tvName = new TextView(getActivity());
+        FrameLayout.LayoutParams tvLp = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        tvLp.gravity = Gravity.BOTTOM;
+        tvName.setGravity(Gravity.CENTER);
+        tvName.setTextColor(getResources().getColor(R.color.live_white));
+        tvName.setPadding(0, 5, 0, 5);
+        tvName.setLines(1);
+        tvName.setBackgroundResource(R.drawable.shape_remote_name_bg);
+        tvName.setLayoutParams(tvLp);
+        flContainer.addView(tvName);
+
+        return flContainer;
     }
 
     @Override
     public void init(Bundle savedInstanceState) {
         super.init(savedInstanceState);
-        presenter.getPlayer().setVideoView((GLTextureView) view);
+        presenter.getPlayer().setVideoView((GLTextureView) flContainer.getChildAt(0));
         gestureDetector = new GestureDetector(getContext(), new MyGestureListener());
         view.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -49,6 +71,11 @@ public class VideoPlayerFragment extends BaseFragment implements VideoPlayerCont
                 return true;
             }
         });
+    }
+
+    @Override
+    public void showCurrentVideoUserName(String name) {
+        tvName.setText(name);
     }
 
     private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
